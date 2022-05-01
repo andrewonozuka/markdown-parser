091cd397ff2d353d5478e50488a3d8371ee2bd82
andrew-onozuka <ryoandrewonozuka@gmail.com>
2022-05-01 04:55:49 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,27 +8,14 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
-
         int currentIndex = 0;
-        while (currentIndex < markdown.length()) {
+        while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-
-            if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1) {
-                currentIndex = markdown.length();
-                break; // prevents infinite loop
-            } else if (closeBracket + 1 == openParen && openBracket != -1) {
-                if (markdown.indexOf("\n", openBracket) < closeParen) {
-                    currentIndex = closeParen;
-                } else if (markdown.charAt(openBracket - 1) == '!') {
-                    currentIndex = closeParen + 1; 
-                } 
-            } else {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
