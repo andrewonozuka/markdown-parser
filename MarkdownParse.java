c9430c40ee8bbb03dba8642e2f0a35d42e6c75d8
andrew-onozuka <ryoandrewonozuka@gmail.com>
2022-05-01 04:58:23 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,8 +24,11 @@ public class MarkdownParse {
                 } else if (markdown.charAt(openBracket - 1) == '!') {
                     currentIndex = closeParen + 1; 
                 }
+            } else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
             }
-            
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
